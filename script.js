83d20fdc76ccd66c3fc452a059add046b7cd2ff5
lucasr98 <lucascalavera@hotmail.com>
2022-08-04 03:20:04 -0300

    04/08/22 (Menu desplegable responsive)

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+var header = document.getElementById("encabezado");
+var menu = document.getElementById("menu");
+
+var check = document.getElementById("check");
+
+let contador = 0;
+
 window.addEventListener("scroll", function(){
-	var header = document.getElementById("encabezado");
 	header.classList.toggle("abajo", window.scrollY> 25);
+	menu.classList.toggle("menu__chico", window.scrollY> 25);
 })
 
 
-
-let logo = document.getElementById("logo");
-let fondo = document.getElementById("fondoImagen")
-
 window.addEventListener('scroll', function(){
-	let value = window.scrollY;
+	var logo = document.getElementById("logo");
+	var fondo = document.getElementById("fondoImagen")
+	var value = window.scrollY;
 	logo.style.marginTop = value * 1.05 + "px";
 	fondo.style.top = value * -0.05 + "px";
 })
+
+function normalizado(){
+	if (contador == 0){
+		header.style.backgroundColor = "var(--color__a1)";
+		contador = 1;
+	}
+	else {
+		header.style.removeProperty("background-color");
+		contador = 0;
+	}
+}
+
+check.addEventListener("click", normalizado, true);
