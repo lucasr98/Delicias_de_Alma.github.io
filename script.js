ece5172464eff44a8a9146868c5ac9fc1cce1f7f
lucasr98 <lucascalavera@hotmail.com>
2022-08-05 06:06:36 -0300

    05/08/22 (Agregado de formulario validado)

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// HTML GENERAL //
+
 var header = document.getElementById("encabezado");
 var menu = document.getElementById("menu");
 
 var check = document.getElementById("check");
 
-let contador = 0;
+let contadorNormalizado = 0;
 
 window.addEventListener("scroll", function(){
-	header.classList.toggle("abajo", window.scrollY> 25);
-	menu.classList.toggle("menu__chico", window.scrollY> 25);
+	header.classList.toggle("abajo", window.scrollY);
+	menu.classList.toggle("menu__chico", window.scrollY);
 })
 
 
@@ -20,14 +22,65 @@ window.addEventListener('scroll', function(){
 })
 
 function normalizado(){
-	if (contador == 0){
+	if (contadorNormalizado == 0){
 		header.style.backgroundColor = "var(--color__a1)";
-		contador = 1;
+		header.style.boxShadow = "0px 0px .5rem 0px rgba(0, 0, 0, .75)";
+		contadorNormalizado = 1;
 	}
 	else {
 		header.style.removeProperty("background-color");
-		contador = 0;
+		header.style.removeProperty("box-shadow");
+		contadorNormalizado = 0;
 	}
 }
 
 check.addEventListener("click", normalizado, true);
+
+// HTML GENERAL //
+
+
+
+// CONTACT //
+
+const name = document.getElementById("contactName"),
+email = document.getElementById("contactEmail"),
+text = document.getElementById("contactTextArea");
+
+const form = document.getElementById("contactInput");
+
+const warnN = document.getElementById("contactWarningN");
+const warnE = document.getElementById("contactWarningE");
+const warnT = document.getElementById("contactWarningT");
+const warn = document.getElementById("contactWarning");
+
+let contador = 0;
+
+form.addEventListener("submit", e=>{
+	e.preventDefault()
+	let warningN = ""
+	let warningE = ""
+	let warningT = ""
+	let entrar = false
+	let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+	warn.innerHTML = ""
+	if (name.value.length < 3){
+		warningN = "El nombre no es válido.";
+		entrar = true
+	}
+	if (!regexEmail.test(email.value)){
+		warningE = "El e-mail no es válido.";
+		entrar = true
+	}
+	if (text.value.length < 50){
+		warningT = "El mensaje es muy corto.";
+		entrar = true
+	}
+	if (entrar){
+		warnN.innerHTML = warningN;
+		warnE.innerHTML = warningE;
+		warnT.innerHTML = warningT;
+	}
+	else {
+		warn.innerHTML = "¡Mensaje enviado!";
+	}
+})
